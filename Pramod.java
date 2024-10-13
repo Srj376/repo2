8f5969290bad455bc1967bce39c4cae3f02575f5
srj <https://github.com/Srj376/srj.git>
2024-10-13 23:58:14 +0530

    third commit

--- Pramod.java
+++ Pramod.java
@@ -4,7 +4,7 @@ public class Pramod {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Pramod");
+		System.out.println("Hello chapri Pramod");
 
 	}
 
